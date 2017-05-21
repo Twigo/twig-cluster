e705d4e9714d40d4d289df59a3dd1712c8b50aa5
budleigh <arguewithplato@gmail.com>
2017-05-20 21:19:12 -0700

    cross-origin stuff

--- src/server/index.js
+++ src/server/index.js
@@ -24,6 +24,17 @@ const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    if (req.method === 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
+});
+
 app.use('/', api);
 
 mongoose.Promise = Promise;
